2520671079bee54e4e89985158d211093f16612a
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-10-02 18:41:03 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -223,11 +223,11 @@ def main():
     if os.path.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("Restarted successfully! \n\n<b>#DynoRestart</b>", chat_id, msg_id)
+        bot.edit_message_text("Restarted successfully!\n\n<b>#DynoRestart</b>", chat_id, msg_id)
         os.remove(".restartmsg")
     elif OWNER_ID:
         try:
-            text = "<b>Bot Restarted!</b> \n\n <b>#DynoRestart</b>"
+            text = "<b>Bot Restarted!</b>\n\n<b>#Dyno_Restarted</b>"
             bot.sendMessage(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
             if AUTHORIZED_CHATS:
                 for i in AUTHORIZED_CHATS:
