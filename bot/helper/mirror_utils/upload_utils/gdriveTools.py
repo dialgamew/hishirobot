b0facd56d0af7c616407cf30532ce774df574b34
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-10-02 16:00:10 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -652,9 +652,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Hishiro Search',
-                                 author_name='Hishiro Bot',
-                                 author_url='https://github.com/hyPnOtICDo0g/hishirobot',
+                                 title = 'Formalin Search',
+                                 author_name='Formalin Bot',
+                                 author_url='https://t.me/fateverse',
                                  html_content=content)
         return
 
@@ -849,9 +849,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                    title = 'Hishiro Search',
-                                                    author_name='Hishiro Bot',
-                                                    author_url='https://github.com/hyPnOtICDo0g/hishirobot',
+                                                    title = 'Formalin Search',
+                                                    author_name='Formalin Bot',
+                                                    author_url='https://t.me/fateverse',
                                                     html_content=content
                                                     )['path'])
 
@@ -930,9 +930,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(telegra_ph.create_page(
-                                                    title = 'Hishiro Search',
-                                                    author_name='Hishiro Bot',
-                                                    author_url='https://github.com/hyPnOtICDo0g/hishirobot',
+                                                    title = 'Formalin Search',
+                                                    author_name='Formalin Bot',
+                                                    author_url='https://t.me/fateverse',
                                                     html_content=content )['path'])
 
         self.num_of_path = len(self.path)      
